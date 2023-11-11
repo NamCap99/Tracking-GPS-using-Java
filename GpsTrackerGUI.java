f28a989315876647cc9a183db6a895fe33722839
NamCap99 <capmanhnam@gmail.com>
2023-11-11 10:47:45 +1030

    update GpsTrackerGUI for more test cases

--- GpsTrackerGUI.java
+++ GpsTrackerGUI.java
@@ -1,10 +1,29 @@
+import nz.sodium.*;
+import swidgets.*;
 import javax.swing.*;
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import swidgets.SLabel;  // Import your custom SLabel
-import nz.sodium.Cell;   // Import from Sodium library
+import java.awt.*;
 
 public class GpsTrackerGUI {
+
+    // Assuming you have a class GpsEvent with the required methods
+    public static class GpsEvent {
+        private final String trackerId;
+        private final double latitude;
+        private final double longitude;
+        private final double altitude;
+
+        public GpsEvent(String trackerId, double latitude, double longitude, double altitude) {
+            this.trackerId = trackerId;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.altitude = altitude;
+        }
+
+        public String getTrackerId() { return trackerId; }
+        public double getLatitude() { return latitude; }
+        public double getLongitude() { return longitude; }
+        public double getAltitude() { return altitude; }
+    }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> createAndShowGUI());
@@ -15,20 +34,57 @@ public class GpsTrackerGUI {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
 
-        // Create the tracker panel
-        JPanel trackerPanel = new JPanel(new GridLayout(0, 3)); // Adjust layout as needed
+        // Panel for tracker data
+        JPanel trackerPanel = new JPanel(new GridLayout(0, 3));
         frame.add(trackerPanel, BorderLayout.NORTH);
 
         // Simulate tracker data
-        Cell<String> trackerData = new Cell<>("Tracker Data Here");
-
-        // Create and add an SLabel to the panel
+        Cell<String> trackerData = new Cell<>("Tracker 1: 0.0, 0.0, 0.0"); // Example tracker data
         SLabel trackerLabel = new SLabel(trackerData);
         trackerPanel.add(trackerLabel);
 
-        // TODO: Add more components and logic
+        // Input panel for latitude and longitude
+        JPanel inputPanel = new JPanel(new FlowLayout());
+        STextField latitudeField = new STextField("Latitude");
+        STextField longitudeField = new STextField("Longitude");
+        SButton applyButton = new SButton("Apply Filter");
+        inputPanel.add(new JLabel("Latitude:"));
+        inputPanel.add(latitudeField);
+        inputPanel.add(new JLabel("Longitude:"));
+        inputPanel.add(longitudeField);
+        inputPanel.add(applyButton);
+        frame.add(inputPanel, BorderLayout.CENTER);
+
+        // Display for combined GPS data
+        STextArea combinedDataDisplay = new STextArea("");
+        frame.add(combinedDataDisplay, BorderLayout.SOUTH);
+
+        // Apply filter logic on button click
+        applyButton.sClicked.listen(ignored -> {
+            double lat = Double.parseDouble(latitudeField.text.sample());
+            double lon = Double.parseDouble(longitudeField.text.sample());
+
+            // Filter logic (assuming a method to get GPS event stream)
+            // Stream<GpsEvent> filteredStream = getFilteredGpsEventStream(lat, lon);
+
+            // Update display (assuming a method to format GPS event data)
+            // Cell<String> displayData = getDisplayDataFromStream(filteredStream);
+            // combinedDataDisplay.setText(displayData.sample());
+        });
 
         frame.pack();
         frame.setVisible(true);
+    }
+
+    // Placeholder method to get filtered GPS event stream
+    private static Stream<GpsEvent> getFilteredGpsEventStream(double latitude, double longitude) {
+        // Implement actual filtering logic here
+        return new Stream<>();
+    }
+
+    // Placeholder method to format GPS event data for display
+    private static Cell<String> getDisplayDataFromStream(Stream<GpsEvent> stream) {
+        // Implement logic to convert stream data to string for display
+        return new Cell<>("");
     }
 }
