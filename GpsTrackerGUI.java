c8761a697f4423854e8096a973b506824317a27d
NamCap99 <capmanhnam@gmail.com>
2023-11-11 19:39:59 +1030

    update for GpsTrackerGUI.java 2.0

--- GpsTrackerGUI.java
+++ GpsTrackerGUI.java
@@ -2,6 +2,7 @@ import nz.sodium.*;
 import swidgets.*;
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class GpsTrackerGUI {
 
@@ -25,6 +26,28 @@ public class GpsTrackerGUI {
         public double getAltitude() { return altitude; }
     }
 
+    
+    private static Cell<String> simulateTrackerData(String trackerId) {
+        // Simulate GPS data for a tracker (latitude, longitude, altitude)
+        Random rand = new Random();
+        double latitude = -90 + 180 * rand.nextDouble();  // Random latitude
+        double longitude = -180 + 360 * rand.nextDouble(); // Random longitude
+        double altitude = 1000 * rand.nextDouble(); // Random altitude
+    
+        // Create a GpsEvent and format it as a string
+        GpsEvent event = new GpsEvent(trackerId, latitude, longitude, altitude);
+        String dataString = "Tracker " + event.getTrackerId() +
+                            ": Lat " + event.getLatitude() +
+                            ", Lon " + event.getLongitude() +
+                            ", Alt " + event.getAltitude();
+    
+        // Create a CellSink and immediately send the data string to it
+        CellSink<String> trackerDataCell = new CellSink<>(dataString);
+    
+        return trackerDataCell;
+    }
+    
+
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> createAndShowGUI());
     }
@@ -35,8 +58,16 @@ public class GpsTrackerGUI {
         frame.setLayout(new BorderLayout());
 
         // Panel for tracker data
-        JPanel trackerPanel = new JPanel(new GridLayout(0, 3));
+        JPanel trackerPanel = new JPanel(new GridLayout(0, 1));
         frame.add(trackerPanel, BorderLayout.NORTH);
+
+        // Simulate and display data for multiple trackers
+        int numberOfTrackers = 5; // Example number of trackers
+        for (int i = 0; i < numberOfTrackers; i++) {
+            Cell<String> trackerData = simulateTrackerData("Tracker " + (i + 1));
+            SLabel trackerLabel = new SLabel(trackerData);
+            trackerPanel.add(trackerLabel);
+        }
 
         // Simulate tracker data
         Cell<String> trackerData = new Cell<>("Tracker 1: 0.0, 0.0, 0.0"); // Example tracker data
