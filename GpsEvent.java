7c00a25952b3f6bba8ce8ac67cbbe1b80db1c2aa
NamCap99 <capmanhnam@gmail.com>
2023-11-13 03:28:26 +1030

    fix privite to public for GpsEvent.java class

--- GpsEvent.java
+++ GpsEvent.java
@@ -1,9 +1,9 @@
 public class GpsEvent {
 
-    private final String trackerId;
-    private final double latitude;
-    private final double longitude;
-    private final double altitude;
+    public String trackerId;
+    public double latitude;
+    public double longitude;
+    public double altitude;
 
     public GpsEvent(String trackerId, double latitude, double longitude, double altitude) {
         this.trackerId = trackerId;
