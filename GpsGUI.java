0d5b63f4fd3dc7e1a0968cfba6b374114c075c56
NamCap99 <capmanhnam@gmail.com>
2023-11-13 08:25:40 +1030

    GpsEvent in GUI update

--- GpsGUI.java
+++ GpsGUI.java
@@ -14,6 +14,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class GpsGUI {
+    public GpsEvent event;
     private static Map<String, JLabel> trackerDistanceLabels = new HashMap<>();
     private static Map<String, GpsEvent> lastKnownPositions = new HashMap<>();
     private static Map<String, Double> trackerDistances = new HashMap<>();
